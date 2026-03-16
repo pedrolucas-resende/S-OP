9854b699da90fe00fca7ee7f557d659ef0dfbf9c
pedrolucas-resende <pedrolucasresende.dev@gmail.com>
2026-03-16 17:06:48 -0300

    Reseta sandbox

--- sandbox.py
+++ sandbox.py
@@ -2,33 +2,12 @@
 from bigquery_client import *
 
 query_validacao = """
-WITH base AS (
-SELECT
-    lugarId,
-    lugar,
-    DATE_TRUNC(inicioData, MONTH) AS mes_ano,
-    COUNT(*) AS soma_contratos,
-    SUM(CASE 
-        WHEN status_contrato = 'Churn' THEN 1 
-        ELSE 0 
-    END) AS soma_churn
-FROM `grw_contrato.contrato`
-WHERE inicioData >= DATE_SUB(CURRENT_DATE(), INTERVAL 36 MONTH)
-GROUP BY lugarId, lugar, mes_ano
-)
-
-SELECT
-    lugarId,
-    lugar,
-    mes_ano,
-    soma_contratos - soma_churn AS motos_alugadas
-FROM base
-ORDER BY mes_ano, motos_alugadas DESC
+    -- Exemplo de query para validação, pode ser qualquer coisa que você queira testar:w
 """
 
 df = rodar_query(query_validacao)
 
 import os
 os.makedirs("data/raw", exist_ok=True)
-df.to_csv("data/raw/motos_alugadas_ultimos_36_meses.csv", index=False)
-print("💾 Salvo em: data/raw/motos_alugadas_ultimos_36_meses.csv")
+df.to_csv("data/raw/sandbox.csv", index=False)
+print("💾 Salvo em: data/raw/sandbox.csv")
